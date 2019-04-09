cb95ddcbeb41c13ed9d46d2c260db7c0b57ee024
Tomppa <tomi.pohja@student.hamk.fi>
2019-04-09 09:34:20 +0300

    Lisää ja muokkaa kysymys tehty

--- vaalikones/src/main/java/vaalikone/LisaaKysymysHandler.java
+++ vaalikones/src/main/java/vaalikone/LisaaKysymysHandler.java
@@ -13,16 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class MuokkaaKysymysHandler
+ * Servlet implementation class LisaaKysymysHandler
  */
-@WebServlet("/MuokkaaKysymysHandler")
-public class MuokkaaKysymysHandler extends HttpServlet {
+@WebServlet("/LisaaKysymysHandler")
+public class LisaaKysymysHandler extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MuokkaaKysymysHandler() {
+    public LisaaKysymysHandler() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,15 +31,15 @@ public class MuokkaaKysymysHandler extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		Connection conn;
 		Statement stm;
 		
-		String lisaaKysymys = request.getParameter("lisaaKysymys");
+		String lisaa_kysymys = request.getParameter("lisaaKysymys");
 		
-		String Kysymys_id = request.getParameter("kysymys_id");
-		String Kysymys = request.getParameter("kysymys");
+		String kysymys_id = request.getParameter("kysymys_id");
+		String Kysymys = request.getParameter("Kysymys");
 		
-
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 
@@ -47,20 +47,17 @@ public class MuokkaaKysymysHandler extends HttpServlet {
 			stm = conn.createStatement();
 			request.getRequestDispatcher("/AdminPage.jsp").forward(request, response);
 			System.out.println("success");
-			String sql = "INSERT INTO kysymykset (KYSYMYS_ID, KYSYMYS)"
-			+ "VALUES (?, ?)"
-			+ "ON DUPLICATE KEY UPDATE "
-			+ " KYSYMYS_ID = VALUES(KYSYMYS_ID), KYSYMYS = VALUES(KYSYMYS)";
+			String sql = "INSERT INTO kysymykset (KYSYMYS_ID, KYSYMYS) VALUES (?, ?)";
 			System.out.println("1");
 			PreparedStatement statement = conn.prepareStatement(sql);
 			System.out.println("2");
-			statement.setString(1, Kysymys_id);
+			statement.setString(1, kysymys_id);
 			statement.setString(2, Kysymys);
 
 			System.out.println("3");
 			statement.executeUpdate();
-			System.out.println("Kysymyksen tiedot päivitetty onnistuneesti");
-			response.sendRedirect(request.getContextPath() + "/MuokkaaKysymysHandler.jsp");
+			System.out.println("Uusi Kysymys lisätty onnistuneesti");
+			response.sendRedirect(request.getContextPath() + "/LisaaKysymys.jsp");
 			conn.close();
 
 		} catch (Exception ex) {
---
+++ vaalikones/src/main/webapp/LisaaKysymys.jsp
@@ -0,0 +1,68 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="vaalikone.MuokkaaKysymysHandler"%>
+<%@page import="persist.Vastaukset"%>
+<%@page import="java.util.List"%>
+<%@page import="persist.Ehdokkaat"%>
+<%@page import="java.sql.*" %>
+<%@page import="java.io.PrintWriter" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>>Lisaa kysymys</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<%String uname =(String)session.getAttribute("uname");
+if(null!=uname){%>
+<h2>Lisää uusi kysymys</h2>
+<div>
+<%
+try {
+	Class.forName("com.mysql.jdbc.Driver");
+	String url="jdbc:mysql://localhost/vaalikone";
+	String username="root";
+	String password="";
+	String query = "SELECT * FROM kysymykset";
+	Connection con = DriverManager.getConnection(url,username,password);
+	Statement stmt = con.createStatement();
+	ResultSet rs = stmt.executeQuery(query);
+	PrintWriter iiii = response.getWriter();
+	while (rs.next()) {
+		%>
+		<tr>
+		<th><%iiii.print(rs.getInt("KYSYMYS_ID")+ "\n");%></th>
+		</tr>
+		<tr>
+		<th><%iiii.print(rs.getString("KYSYMYS")+ "\n" + "<br>");%></th>
+		</tr>
+			<%
+	}
+	%>
+		<%
+		stmt.close();
+}
+	
+ catch (Exception e) {
+}
+
+%>
+
+<form action="LisaaKysymysHandler" method="POST">
+
+Kysymys ID<input type="text" name="kysymys_id"/><br>
+Kysymys<input type="text" name="kysymys"/><br>
+
+<input type="submit" value='lisaaKysymys'/>
+
+</form>
+</div>
+
+
+
+</body>
+</html>
+<% }else{
+		response.sendRedirect("http://localhost:8080/index.html");
+	} %>
