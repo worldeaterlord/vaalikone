e1ec99cd5aee8d7581d948c8601ab3d5e03176ac
acoustic <antti.kojo@hotmail.com>
2019-04-10 12:44:55 +0300

    a

--- vaalikones/src/main/java/vaalikone/LisaaKysymysHandler.java
+++ vaalikones/src/main/java/vaalikone/LisaaKysymysHandler.java
@@ -19,18 +19,20 @@ import javax.servlet.http.HttpServletResponse;
 public class LisaaKysymysHandler extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LisaaKysymysHandler() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LisaaKysymysHandler() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		Connection conn;
 		Statement stm;
@@ -64,11 +66,12 @@ public class LisaaKysymysHandler extends HttpServlet {
 
 	}
 
-
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
