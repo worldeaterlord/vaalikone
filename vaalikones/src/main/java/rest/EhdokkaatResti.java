bb04a2131289624ac985d98c837791eea48ac69b
Tomppa <tomi.pohja@student.hamk.fi>
2019-04-23 12:15:31 +0300

    didiidid

---
+++ vaalikones/src/main/java/rest/EhdokkaatResti.java
@@ -0,0 +1,54 @@
+package rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.validation.constraints.Size;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import persist.Ehdokkaat;
+
+@Path("/riistaservice")
+public class EhdokkaatResti {
+
+	@GET
+	@Path("/riista")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Ehdokkaat getEhdokasId() {
+		System.out.println("Hei vain");
+		Ehdokkaat r=new Ehdokkaat();
+		return r;
+		
+		
+	}
+	@POST
+	@Path("/ehdokas")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Ehdokkaat postEhdokas(Ehdokkaat r) {
+		EntityManagerFactory emf=Persistence.createEntityManagerFactory("vaalikones");
+		EntityManager em=emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(r);
+		em.getTransaction().commit();
+		
+		System.out.println(r.getEhdokasId());
+		r.getEhdokasId();
+		r.getSukunimi();
+		r.getEtunimi();		
+		return r;
+	}
+
+}
