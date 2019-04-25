68934d1569e25ca164bd6581c0ada727b5379bb9
Tomppa <tomi.pohja@student.hamk.fi>
2019-04-25 10:58:34 +0300

    Lisätty lisääEhdokas-->Toiminta ei vielä kunnossa

--- vaalikones/src/main/java/rest/EhdokkaatResti.java
+++ vaalikones/src/main/java/rest/EhdokkaatResti.java
@@ -46,9 +46,23 @@ public class EhdokkaatResti {
 				+ "</a>"
 				+ "<br>"
 				+ "<br>"
+				+ "Poista ehdokas"
 				+ "<form action='http://localhost:8080/rest/ehd/poista' method='post'/>"
 				+ "<input type='text' name='poista' />"
 				+ "<input type='submit' value='delete'"
+				+ "<br>"
+				+ "<br>"
+				+ "Uusi ehdokas"
+				+ "<br>"
+				+ "<form action='http://localhost:8080/rest/ehd/lisaaSivu' method='post'/>"
+				+ "<input type='submit' value='add'"
+				+ "<br>"
+				+ "<br>"
+				+ "Muokkaa ehdokasta"
+				+ "<br>"
+				+ "<form action='http://localhost:8080/rest/ehd/edit' method='post'/>"
+				+ "<input type='text' name='muokkaa' />"
+				+ "<input type='submit' value='edit'"
 				+ "</body>"
 				;
 		return b;
@@ -75,5 +89,82 @@ public class EhdokkaatResti {
 			Query query = em.createNamedQuery(kysely);
 			query.executeUpdate();
 
+	}
+	@GET
+	@Path("/lisaaSivu")
+	@Produces(MediaType.TEXT_HTML)
+	public String lisaaYksi() {
+
+			
+			String b = "<html>"
+					+ "<head>"
+					+"</head>"
+					+ "Anna uuden ehdokkaan tiedot: "
+					+ "<br>"
+					+ "<form action='http://localhost:8080/rest/ehd/lisaa' method='post'/>"
+					+ "<br>"
+					+ "Ehdokas id"
+					+ "<br>"
+					+ "<input type='text' name='EhdokasId' />"
+					+ "<br>"
+					+ "Sukunimi"
+					+ "<br>"
+					+ "<input type='text' name='Sukunimi' />"
+					+ "<br>"
+					+ "Etunimi"
+					+ "<br>"
+					+ "<input type='text' name='Etunimi' />"
+					+ "<br>"
+					+ "Ikä"
+					+ "<br>"
+					+ "<input type='text' name='Ika' />"
+					+ "<br>"
+					+ "Puolue"
+					+ "<br>"
+					+ "<input type='text' name='Puolue' />"
+					+ "<br>"
+					+ "Kotipaikkakunta"
+					+ "<br>"
+					+ "<input type='text' name='Kotipaikkakunta' />"
+					+ "<br>"
+					+ "Miksi Eduskuntaan"
+					+ "<br>"
+					+ "<input type='text' name='MiksiEduskuntaan' />"
+					+ "<br>"
+					+ "Mitä asioita haluat edistää"
+					+ "<br>"
+					+ "<input type='text' name='MitaAsioitaHaluatEdistaa' />"
+					+ "<br>"
+					+ "Ammatti"
+					+ "<br>"
+					+ "<input type='text' name='Ammatti' />"
+					+ "<br>"
+					+ "<br>"
+					+ "<input type='submit' value='add'"
+					+ "<br>"
+					+ "<br>"
+					+ "</body>"
+					;
+			return b;
+	}
+	
+
+	@GET
+	@Path("/LisaaSivu/{param}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void LisaaEhdokas(@PathParam("param") Integer EhdokasId, String Sukunimi, String Etunimi, Integer Ika,String Puolue,String Kotipaikkakunta, String MiksiEduskuntaan, String MitaAsioitaHaluatEdistaa, String Ammatti ) {
+			EntityManager em = EmClass.getEm();
+			String kysely = "INSERT INTO ehdokkaat (EHDOKAS_ID, SUKUNIMI, ETUNIMI, IKA, PUOLUE, KOTIPAIKKAKUNTA, MIKSI_EDUSKUNTAAN, MITA_ASIOITA_HALUAT_EDISTAA, AMMATTI) VALUES (?, ?, ?, ?, ?, ? ,? ,? ,?)";
+			Query query = em.createNamedQuery(kysely);
+			query.setParameter(1, EhdokasId);
+			query.setParameter(2, Sukunimi);
+			query.setParameter(3, Etunimi);
+			query.setParameter(4, Ika);
+			query.setParameter(5, Puolue);
+			query.setParameter(6, Kotipaikkakunta);
+			query.setParameter(7, MiksiEduskuntaan);
+			query.setParameter(8, MitaAsioitaHaluatEdistaa);
+			query.setParameter(9, Ammatti);
+			query.executeUpdate();
 	}
 }
