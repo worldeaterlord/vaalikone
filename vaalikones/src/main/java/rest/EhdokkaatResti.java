87b169705df96ca5057d914d8204fb6b739288de
acoustic <antti.kojo@hotmail.com>
2019-04-26 12:08:36 +0300

    Revert "Update EhdokkaatResti.java"
    This reverts commit a72e62b669f0966aadc10fad2a0504cd6bcf6d5a.

--- vaalikones/src/main/java/rest/EhdokkaatResti.java
+++ vaalikones/src/main/java/rest/EhdokkaatResti.java
@@ -79,7 +79,7 @@ public class EhdokkaatResti {
 		try {
 		EntityManager em = EmClass.getEm();
 		em.getTransaction().begin();
-		em.persist(e);
+		em.merge(e);
 		em.getTransaction().commit();
 		}catch(Exception t){
 			System.out.println(e.getEtunimi());
